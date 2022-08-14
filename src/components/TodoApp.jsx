13fb9a3ddbf1593ea3da8f8997b1242f3f4b45db
Anibal Robleto <anibaldelhi@outlook.com>
2022-08-14 13:38:40 -0600

    Add Todo Reducer

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,12 +1,32 @@
+import { TodoList } from './TodoList';
+import { todoReducer } from './todoReducer';
+import { useReducer } from 'react';
+
+const initialState = [
+    {
+        id : 1,
+        description: "Primer tarea"
+    },
+    {
+        id: 2,
+        description: "Segunda tarea"
+    }
+]
+
 export const TodoApp = () =>{
+
+const [todos, dispatch] = useReducer( todoReducer , initialState );
 
     return(
         <div className = "row">
+            <h1>TODO App</h1>
+            <hr />
             <div className="col-8">
-                <h1>Todo List</h1>
+                <h2>Todo List</h2>
+                <TodoList todos = {todos} />
             </div>
             <div className="col-4">
-                <h1>Todo Add</h1>
+                <h2>Todo Add</h2>
             </div>
         </div>
     )
