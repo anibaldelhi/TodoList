c3c8fe19e8c86c919b0fd066050ce2201e0995aa
Anibal Robleto <anibaldelhi@outlook.com>
2022-08-14 17:15:06 -0600

    Implement Todo Item

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,19 +1,16 @@
+import { TodoItem } from "./TodoItem"
+
 export const TodoList = ({todos = []}) => {
 
     return(
-        <>
         <ul className = "list-group">
             {
                 todos.map(
                     todo => (
-                        <li key = {todo.id}>.
-                            <span>Item 1</span>
-                            <button>Borrar</button>
-                        </li>
+                        <TodoItem key={todo.id} todo = {todo} />
                     )
                 )
             }
         </ul>
-        </>
     )
 }
